eff4fc39d9a257ba129e0b6bd9a99019fa035860
sahithiraghupatruni <131184789+sahithiraghupatruni@users.noreply.github.com>
2025-03-17 16:37:12 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,7 @@
+#print multiplication table of a given number
+def multiplication_table(n):
+  for i in range(1,11):
+    print(f"{n}*{i}={n*i}")
+#user input
+n=int(input("Enter a number: ")
+multiplication_table(n)
