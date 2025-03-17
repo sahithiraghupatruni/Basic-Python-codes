caff4f8f057ef857dd8c2fafe77534eb362d0878
sahithiraghupatruni <131184789+sahithiraghupatruni@users.noreply.github.com>
2025-03-17 16:28:20 +0530

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#function to take user input and print a greeting
+def greet_user():
+  name=input("Enter your name: ")
+  print(f"Hello,{name}! Welcome to my Python Learning Repository")
+#call the function
+greet_user()
